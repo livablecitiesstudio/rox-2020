f56d6ff0148dea44009c4e73754442f11dd12a73
livablecitiesstudio <ylo@livable-cities.com>
2020-04-08 15:55:37 -0600

    upload images

--- js/script.js
+++ js/script.js
@@ -401,3 +401,9 @@
 
 let headerNav = document.querySelector('header nav ul');
 // headerNav.innerHTML = '';
+
+window.onscroll = function(){
+    // window.location.href = window.location.pathname;
+    // window.location.href.split('#')[0];
+    if (window.location.hash) window.location.hash = "";
+}
